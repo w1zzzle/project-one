de461985b213cc793646da7ea2faf4839f1da428
DimaGenkai <92301623+w1zzzle@users.noreply.github.com>
2023-06-08 14:37:14 +0400

    Добавьте файлы проекта.

---
+++ project-one/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project_one.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
